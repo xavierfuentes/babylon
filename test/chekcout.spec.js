d450da815bfdb9a7d72e43337eb33b2dfc94c725
Xavi <xavifuefer@gmail.com>
2016-09-20 02:05:15 +0100

    new scenarios

--- test/chekcout.spec.js
+++ test/chekcout.spec.js
@@ -2,25 +2,35 @@ var expect = require('chai').expect;
 var Checkout = require('../checkout');
 
 describe('Checkout', () => {
-  it('should have a "scan" public method', () => {
-    const co = new Checkout();
-    expect(co).to.respondTo('scan');
-  });
 
-  it('should have a "total" public method', () => {
-    const co = new Checkout();
-    expect(co).to.respondTo('total');
-  });
+  describe('Structure', () => {
+    it('should have a "scan" public method', () => {
+      const co = new Checkout();
+      expect(co).to.respondTo('scan');
+    });
 
-  it('should scan items and store them', () => {
-    const co = new Checkout();
-    co.scan({code: '001', name: 'Article 1', price: '£1'});
-    co.scan({code: '002', name: 'Article 2', price: '£2'});
-    expect(co.items).to.deep.equal([
-      {code: '001', name: 'Article 1', price: '£1'},
-      {code: '002', name: 'Article 2', price: '£2'}
-    ]);
-  });
+    it('should have a "total" public method', () => {
+      const co = new Checkout();
+      expect(co).to.respondTo('total');
+    });
+  })
+
+  describe('Basic functionality', () => {
+    let co;
+
+    beforeEach(() => {
+      co = new Checkout();
+      co.scan({code: '001', name: 'Article 1', price: '£1'});
+      co.scan({code: '002', name: 'Article 2', price: '£2'});
+    })
+
+    it('should scan items and store them', () => {
+      expect(co.items).to.deep.equal([
+        {code: '001', name: 'Article 1', price: '£1'},
+        {code: '002', name: 'Article 2', price: '£2'}
+      ]);
+    });
+  })
 
   it('should calculate the total price');
   it('should apply 10% discount if you spend over £60');
