9483e2d1c0894a9b8bd9c89aecd9ca73be4e50d7
Xavi <xavifuefer@gmail.com>
2016-09-20 01:37:28 +0100

    first round of pending tests

---
+++ test/chekcout.spec.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var checkout = require('../checkout');
+
+describe('Checkout', () => {
+  it('should have a "scan" public method';
+  it('should have a "total" public method');
+  it('should calculate the total price');
+  it('should apply 10% discount if you spend over £60');
+  it('should drop the price of "lavender hearts" to £8.5 when buying 2 or more');
+  it('should apply custom promotional rules');
+})
