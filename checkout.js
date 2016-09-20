8c4c4841a628aab606a56743277f774e4bdeaaaf
Xavi <xavifuefer@gmail.com>
2016-09-20 02:48:06 +0100

    should apply 10% discount if you spend over £60

--- checkout.js
+++ checkout.js
@@ -1,5 +1,11 @@
 class Checkout {
-  constructor() {
+  constructor(promotional_rules = {
+    total: { discount: 10, minimum: 60 },
+    items: {
+      '001': { price: 8.5, minimum: 2 },
+    }
+  }) {
+    this.promotional_rules = promotional_rules;
     this.items = [];
   }
 
@@ -9,9 +15,15 @@ class Checkout {
   }
 
   total() {
-    return this.items.reduce((total, item) => {
+    let totalPreDiscount = this.items.reduce((total, item) => {
       return total + Number(item.price.replace(/^£/, ""));
     }, 0);
+
+    // apply a discount to the total price if necessary
+    if (totalPreDiscount >= this.promotional_rules.total.minimum)
+      totalPreDiscount -= totalPreDiscount * this.promotional_rules.total.discount / 100
+
+    return Math.round(totalPreDiscount * 100) / 100; // Round to at most 2 decimal places
   }
 }
 
--- test/chekcout.spec.js
+++ test/chekcout.spec.js
@@ -16,13 +16,9 @@ describe('Checkout', () => {
   })
 
   describe('Basic functionality', () => {
-    let co;
-
-    beforeEach(() => {
-      co = new Checkout();
-      co.scan({code: '001', name: 'Article 1', price: '£1'});
-      co.scan({code: '002', name: 'Article 2', price: '£2'});
-    })
+    let co = new Checkout();
+    co.scan({code: '001', name: 'Article 1', price: '£1'});
+    co.scan({code: '002', name: 'Article 2', price: '£2'});
 
     it('should scan items and store them', () => {
       expect(co.items).to.deep.equal([
@@ -36,7 +32,19 @@ describe('Checkout', () => {
     });
   })
 
-  it('should apply 10% discount if you spend over £60');
-  it('should drop the price of "lavender hearts" to £8.5 when buying 2 or more');
-  it('should apply custom promotional rules');
+  describe('Standard promotional rules', () => {
+    let co = new Checkout();
+
+    it('should apply 10% discount if you spend over £60', () => {
+      co.scan({code: '002', name: 'Personalised cufflinks', price: '£45'});
+      co.scan({code: '003', name: 'Kids T-shirt', price: '£19.95'});
+      expect(co.total()).to.equal(58.46);
+    });
+
+    it('should drop the price of "lavender hearts" to £8.5 when buying 2 or more');
+  });
+
+  describe('Custom promotional rules', () => {
+    it('should apply custom promotional rules');
+  })
 })
